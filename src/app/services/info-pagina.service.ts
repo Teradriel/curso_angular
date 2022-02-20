a4dc3d8e3db5503136eaf4e88f83a5a6e5d275b2
Luca <luca8973@hotmail.com>
2022-02-21 00:15:58 +0100

    base de datos 1

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { InfoPaginaInt } from '../interfaces/info-pagina.interface';
+import { EquipoInt } from '../interfaces/equipo.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -8,16 +9,31 @@ import { InfoPaginaInt } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info: InfoPaginaInt = {};
   cargada = false;
+  equipo: EquipoInt = {};
 
   constructor(private http: HttpClient) {
     // console.log("Info Pagina");
 
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+  private cargarInfo() {
     //leer el archivo json
     this.http
       .get('/assets/data/data-pagina.json')
       .subscribe((resp: InfoPaginaInt) => {
         this.cargada = true;
         this.info = resp;
+      });
+  }
+  private cargarEquipo() {
+    this.http
+      .get(
+        'https://base-datos-curso-angular-default-rtdb.europe-west1.firebasedatabase.app/equipo.json'
+      )
+      .subscribe((resp: EquipoInt) => {
+        this.cargada = true;
+        this.equipo = resp;
 
         console.log(resp);
       });
